c5f090e904f20b518a36f445602d289c3a308278
scienceopen <scienceopen@users.noreply.github.com>
2015-06-05 10:29:48 -0400

    doc

--- MGSeds.m
+++ MGSeds.m
@@ -2,6 +2,9 @@
 % This file reads downloaded .sri files to plot high-level Mars Global Surveyor
 % radio occultation data from
 % http://pds-geosciences.wustl.edu/missions/mgs/rsdata.html
+%
+% Note, the .lbl format is not nice for parsing. Matlab textscan() can manage
+% it, but Octave 3.8/4.0 cannot. Sorry.
 
 clc, clear all, close all, fclose('all');
 %% find .sri files in directory
@@ -9,6 +12,7 @@ path = 'data/';
 d = dir([path,'*.sri']); 
 fn = {d(:).name};
 
+startDateNum=[]; %in case nothing found
 for i = 1:length(fn)
 try
     fid = fopen([path,fn{i}(1:end-4),'.lbl']);
@@ -26,22 +30,22 @@ elseif ~strcmp(lbl{2}(ObjInd),'IMAGE')
     warning(['File ',fn{i},' is not an image.'])
     continue
 end
-LinInd = find(strcmp(lbl{1},'LINES'),1); NumLines(i) = str2double(lbl{2}(LinInd));
-LinInd = find(strcmp(lbl{1},'LINE_SAMPLES'),1); NumSamp(i) = str2double(lbl{2}(LinInd));
-LinInd = find(strcmp(lbl{1},'OFFSET'),1); Offset(i) = str2double(lbl{2}(LinInd));
-LinInd = find(strcmp(lbl{1},'SCALING_FACTOR'),1); ScaleFact(i) = str2double(lbl{2}{LinInd});
+LinInd = find(strcmp(lbl{1},'LINES'),1); NumLines = str2double(lbl{2}(LinInd));
+LinInd = find(strcmp(lbl{1},'LINE_SAMPLES'),1); NumSamp = str2double(lbl{2}(LinInd));
+LinInd = find(strcmp(lbl{1},'OFFSET'),1); Offset = str2double(lbl{2}(LinInd));
+LinInd = find(strcmp(lbl{1},'SCALING_FACTOR'),1); ScaleFact = str2double(lbl{2}{LinInd});
 
-LinInd = find(strcmp(lbl{1},'START_TIME'),1); StartDate{i} = lbl{2}(LinInd);
+LinInd = find(strcmp(lbl{1},'START_TIME'),1); StartDate = lbl{2}(LinInd);
 %[startY(i), startM(i), startD(i),startH(i), startMN(i), startS(i)] = datevec([StartDate{i}{1}(1:10),' ',StartDate{i}{1}(12:19)],31);
-startDateNum(i) = datenum([StartDate{i}{1}(1:10),' ',StartDate{i}{1}(12:19)],31);
+startDateNum(i) = datenum([StartDate{1}(1:10),' ',StartDate{1}(12:19)],31);
 
-LinInd = find(strcmp(lbl{1},'STOP_TIME'),1); StopDate{i} = lbl{2}(LinInd);
+LinInd = find(strcmp(lbl{1},'STOP_TIME'),1); StopDate = lbl{2}(LinInd);
 %[stopY(i), stopM(i), stopD(i),stopH(i), stopMN(i), stopS(i)] = datevec([StopDate{i}{1}(1:10),' ',StopDate{i}{1}(12:19)],31);
-stopDateNum(i) = datenum([StopDate{i}{1}(1:10),' ',StopDate{i}{1}(12:19)],31);
+stopDateNum(i) = datenum([StopDate{1}(1:10),' ',StopDate{1}(12:19)],31);
 
 %% get data
 fidbin = fopen([path,fn{i}]);
-imgData{i} = fliplr(fread(fidbin, [NumSamp(i),NumLines(i)], 'int16',0,'b').*ScaleFact(i) + Offset(i));
+imgData{i} = fliplr(fread(fidbin, [NumSamp,NumLines], 'int16',0,'b').*ScaleFact + Offset);
 fclose(fidbin);
 xBin = 4.88; %Hz, from .lbl description
 xStart = 0; %Hz
