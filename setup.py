686335418a2f5be6b2e21813c4eb1373c8950e27
scienceopen <scienceopen@users.noreply.github.com>
2016-08-02 11:23:04 -0400

    template

--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 from setuptools import setup
 import subprocess
 
 try:
-    subprocess.run(['conda','install','--yes','--file','requirements.txt'])
+    subprocess.run(['conda','install','--file','requirements.txt'])
 except Exception as e:
-    print('you will need to install packages in requirements.txt  {}'.format(e))
+    pass
 
-with open('README.rst','r') as f:
-	long_description = f.read()
-
-#%% install
 setup(name='mgsutils',
-      version='0.1',
 	  description='Radio Occultation with Mars Global Surveyor',
-	  long_description=long_description,
 	  author='Michael Hirsch',
-      install_requires=[],
+      install_requires=['pathlib2'],
 	  url='https://github.com/scienceopen/mgsutils',
         packages=['mgsutils']
 	  )
