a68688ca1c430318f9020bf29f225274042ce98b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-04-24 04:48:34 -0400

    py27

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-req = ['nose','python-dateutil','pytz','numpy','pandas','matplotlib','seaborn','xarray']
+req = ['nose','python-dateutil','pytz','numpy','pandas','matplotlib','seaborn','xarray','pathlib2']
 # %%
 try:
     import conda.cli
     conda.cli.main('install',*req)
 except Exception as e:
     import pip
-    pip.main(['install',*req])
+    pip.main(['install'] + req)
 # %%
 from setuptools import setup  # enables develop
 
@@ -14,13 +14,14 @@ setup(name='mgsutils',
       packages=['mgsutils'],
       author='Michael Hirsch, Ph.D',
       description='Model of Earth atmosphere absorption and transmission vs. wavelength and location on Earth.',
-      version='1.0.0',
+      version='1.0.1',
       url = 'https://github.com/scivision/mgs-utils',
       classifiers=[
       'Intended Audience :: Science/Research',
       'Development Status :: 4 - Beta',
       'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
       'Topic :: Scientific/Engineering :: Atmospheric Science',
-      'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python',
       ],
 	  )
